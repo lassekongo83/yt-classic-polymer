037fb6dc2d32ab193f7a8d17bb2d499c854f5e3a
lassekongo83 <lassekongo83@users.noreply.github.com>
2020-07-18 12:44:47 +0200

    Updates

--- src/sass/parse-sass.sh
+++ src/sass/parse-sass.sh
@@ -10,3 +10,8 @@ SASSC_OPT="-M -t compact"
 echo Generating the css...
 
 sassc $SASSC_OPT main.scss ../inject/inject.css
+
+# using csso-cli to minify the css file. Can be installed with: npm install -g csso-cli
+csso -i ../inject/inject.css -o ../inject/inject.min.css
+# remove the non-minified css file
+rm ../inject/inject.css
