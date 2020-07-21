c910543ae80e82439f7e98cc3aa9c007c22ac46b
lassekongo83 <lassekongo83@users.noreply.github.com>
2020-07-21 14:03:30 +0200

    Proper disabling of the miniplayer

--- js/contentScript.js
+++ js/contentScript.js
@@ -2,6 +2,22 @@
 document.querySelectorAll('ytd-miniplayer').forEach(el => el.remove());
 document.querySelectorAll('#masthead-ad').forEach(el => el.remove());
 
+// actually disable the miniplayer
+// userscript from u/IStoleThePies
+(function() {
+  document.body.addEventListener("yt-navigate-finish", function(event) {
+    if (document.getElementsByTagName('ytd-miniplayer').length) {
+      document.querySelector('ytd-miniplayer').parentNode.removeChild(document.querySelector('ytd-miniplayer'));
+    }
+    if (document.getElementsByClassName('ytp-miniplayer-button').length) {
+      document.querySelector('.ytp-miniplayer-button').parentNode.removeChild(document.querySelector('.ytp-miniplayer-button'))
+    }
+    if (window.location.pathname != "/watch") {
+      document.querySelector('#movie_player video').parentNode.removeChild(document.querySelector('#movie_player video'));
+    }
+  });
+})();
+
 //insert elements
 // Disabled for now because it needs localization
 /*
