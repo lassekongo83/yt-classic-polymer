356bb25bbae7963f890b7e033d0729e295d03797
lassekongo83 <lassekongo83@users.noreply.github.com>
2020-07-22 20:13:23 +0200

    A couple of minor fixes

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,5 +1,4 @@
 // Remove elements
-document.querySelectorAll('ytd-miniplayer').forEach(el => el.remove());
 document.querySelectorAll('#masthead-ad').forEach(el => el.remove());
 
 // actually disable the miniplayer
