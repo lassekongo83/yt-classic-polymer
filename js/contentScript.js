c242563232b2567d9f0efb497fafed8bab6f3aa5
lassekongo83 <lassekongo83@users.noreply.github.com>
2020-07-30 10:17:25 +0200

    Minor addition

--- js/contentScript.js
+++ js/contentScript.js
@@ -18,7 +18,7 @@ document.querySelectorAll('#masthead-ad').forEach(el => el.remove());
 })();
 
 //insert elements
-// Disabled for now because it needs localization
+// Disabled for now because it needs localization and dark theme crap
 /*
 var items = [{
   text: "Home",
@@ -32,18 +32,26 @@ var items = [{
 }]
 
 function buildList(){
-    var newList = document.createElement("ul");
-    document.getElementById("masthead").appendChild(newList);
+  var newList = document.createElement("ul");
+  newList.className = 'old_menu';
+  document.getElementById("masthead").appendChild(newList);
 
-    items.forEach(function (item) {
-        var li = document.createElement("li");
-        var link = document.createElement("a");
-        link.innerText = item.text;
-        link.href = item.url;
-        li.appendChild(link);
-        newList.appendChild(li)
-    });
+  items.forEach(function (item) {
+    var li = document.createElement("li");
+    var link = document.createElement("a");
+    li.className = 'old_menuitem';
+    link.innerText = item.text;
+    link.href = item.url;
+    li.appendChild(link);
+    newList.appendChild(li)
+  });
 }
+
+const addCSS = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s})(document);
+addCSS(".old_menu{ height:40px; border-bottom:1px solid #e8e8e8; display:flex; justify-content:center; align-items:center;}")
+addCSS(".old_menuitem a{ box-sizing:border-box; color:#666; font-size:13px; text-decoration:none; line-height:40px; font-weight:500; font-family:'YouTube Noto',Roboto,arial,sans-serif;}")
+addCSS(".old_menuitem a:hover{ box-shadow:inset 0 -3px #f00; color:#333;}")
+addCSS(".old_menuitem{ display:inline-flex; flex-direction:column; margin-left:30px; height:40px; justify-content:center; align-items:center;}")
 
 buildList();
 */
