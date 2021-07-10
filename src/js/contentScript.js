56df060b1582e14abaeba104f96c18b97c134de2
lassekongo83 <lassekongo83@users.noreply.github.com>
2021-07-10 07:55:29 +0200

    Appbar smooth navigation #39

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -53,6 +53,15 @@ function addStyle(styleString) {
   document.head.append(style);
 }
 
+// Create script
+function createScript(input, id) {
+  const a = document.body;
+  const b = document.createElement("script");
+  b.text = input;
+  b.setAttribute("id", id);
+  a.appendChild(b);
+}
+
 // Click something
 function clickButton(selector){
   let elm = document.querySelectorAll(selector)[0];
@@ -129,13 +138,15 @@ function listDisplay() {
 }
 
 // Old horizontal navigation bar
-// TODO: Make the navigation buttons use yt navigation instead of reloading the document
 function navBar() {
   // All items we'd like to add
   const navItems = [
-    {href: '/', text: chrome.i18n.getMessage('c_home')},
-    {href: '/feed/trending', text: chrome.i18n.getMessage('c_trending')},
-    {href: '/feed/subscriptions', text: chrome.i18n.getMessage('c_subs')},
+    //{href: '/', text: chrome.i18n.getMessage('c_home')},
+    //{href: '/feed/trending', text: chrome.i18n.getMessage('c_trending')},
+    //{href: '/feed/subscriptions', text: chrome.i18n.getMessage('c_subs')},
+    {text: chrome.i18n.getMessage('c_home')},
+    {text: chrome.i18n.getMessage('c_trending')},
+    {text: chrome.i18n.getMessage('c_subs')},
   ];
 
   // A few variables for use later
@@ -147,7 +158,7 @@ function navBar() {
   for (let i = 0; i < navItems.length; i++) {
     // Create a fresh list item, and anchor
     navItem = document.createElement("li");
-    navLink = document.createElement("a");
+    navLink = document.createElement("span");
 
     // Set properties on anchor
     navLink.href = navItems[i].href;
@@ -178,6 +189,13 @@ function navBar() {
   navList.childNodes[0].className = "ytcp-nav-home ytcp-nav-item";
   navList.childNodes[1].className = "ytcp-nav-trending ytcp-nav-item";
   navList.childNodes[2].className = "ytcp-nav-subs ytcp-nav-item";
+
+  const navHome = document.querySelector('.ytcp-nav-home');
+  const navTrend = document.querySelector('.ytcp-nav-trending');
+  const navSubs = document.querySelector('.ytcp-nav-subs');
+  navHome.addEventListener('click', navigateToHome);
+  navTrend.addEventListener('click', navigateToTrending);
+  navSubs.addEventListener('click', navigateToSubs);
 }
 // Insert the old nav menu again when navigating
 function navBarNavigation() {
@@ -194,11 +212,93 @@ function navBarNavigation() {
     if (nav !== null) {
       nav.remove();
     }
+    const navHome = document.querySelector('.ytcp-nav-home');
+    const navTrend = document.querySelector('.ytcp-nav-trending');
+    const navSubs = document.querySelector('.ytcp-nav-subs');
+    navHome.addEventListener('click', navigateToHome);
+    navTrend.addEventListener('click', navigateToTrending);
+    navSubs.addEventListener('click', navigateToSubs);
   });
 }
 // make room for the nav menu
 function makeRoom() {
   addStyle(`[page-subtype="home"] ytd-two-column-browse-results-renderer,[page-subtype="trending"] ytd-two-column-browse-results-renderer,[page-subtype="subscriptions"] ytd-two-column-browse-results-renderer{margin-top:60px!important;} [page-subtype="trending"] tp-yt-app-header{top:60px!important;}`);
+}
+// Smooth navigation to homepage, trending, and subscriptions
+// by https://github.com/Prometheus2206
+function navigateToHome() {
+  document.querySelector('ytd-app').insertAdjacentHTML('beforeend',
+  '<ytd-guide-entry-renderer class="ytd-guide-entry-renderer navigateToHomeTemp"></ytd-guide-entry-renderer>');
+  createScript(
+  `document.querySelector('.navigateToHomeTemp').data = {};
+  document.querySelector('.navigateToHomeTemp').data.navigationEndpoint = {
+    "clickTrackingParams": "CJMBELUsGAAiEwi3y-qlp9bxAhVchuUHHSLSCts=",
+    "commandMetadata": {
+      "webCommandMetadata": {
+        "url": "/",
+        "webPageType": "WEB_PAGE_TYPE_BROWSE",
+        "rootVe": 3854,
+        "apiUrl": "/youtubei/v1/browse"
+      }
+    },
+    "browseEndpoint": {
+      "browseId": "FEwhat_to_watch"
+    }
+  };`,
+  'polymerNavigationScript');
+  document.querySelector('.navigateToHomeTemp').click();
+  document.querySelector('.navigateToHomeTemp').remove();
+  document.querySelector('#polymerNavigationScript').remove();
+}
+
+function navigateToTrending() {
+  document.querySelector('ytd-app').insertAdjacentHTML('beforeend',
+  '<ytd-guide-entry-renderer class="ytd-guide-entry-renderer navigateToTrendingTemp"></ytd-guide-entry-renderer>');
+  createScript(
+  `document.querySelector('.navigateToTrendingTemp').data = {};
+  document.querySelector('.navigateToTrendingTemp').data.navigationEndpoint = {
+    "clickTrackingParams": "CJIBELUsGAEiEwi3y-qlp9bxAhVchuUHHSLSCts=",
+    "commandMetadata": {
+      "webCommandMetadata": {
+        "url": "/feed/trending",
+        "webPageType": "WEB_PAGE_TYPE_BROWSE",
+        "rootVe": 6827,
+        "apiUrl": "/youtubei/v1/browse"
+      }
+    },
+    "browseEndpoint": {
+      "browseId": "FEexplore"
+    }
+  };`,
+  'polymerNavigationScript');
+  document.querySelector('.navigateToTrendingTemp').click();
+  document.querySelector('.navigateToTrendingTemp').remove();
+  document.querySelector('#polymerNavigationScript').remove();
+}
+
+function navigateToSubs() {
+  document.querySelector('ytd-app').insertAdjacentHTML('beforeend',
+  '<ytd-guide-entry-renderer class="ytd-guide-entry-renderer navigateToSubsTemp"></ytd-guide-entry-renderer>');
+  createScript(
+  `document.querySelector('.navigateToSubsTemp').data = {};
+  document.querySelector('.navigateToSubsTemp').data.navigationEndpoint = {
+    "clickTrackingParams": "CJEBELUsGAIiEwi3y-qlp9bxAhVchuUHHSLSCts=",
+    "commandMetadata": {
+      "webCommandMetadata": {
+        "url": "/feed/subscriptions",
+        "webPageType": "WEB_PAGE_TYPE_BROWSE",
+        "rootVe": 6827,
+        "apiUrl": "/youtubei/v1/browse"
+      }
+    },
+    "browseEndpoint": {
+      "browseId": "FEsubscriptions"
+    }
+  };`,
+  'polymerNavigationScript');
+  document.querySelector('.navigateToSubsTemp').click();
+  document.querySelector('.navigateToSubsTemp').remove();
+  document.querySelector('#polymerNavigationScript').remove();
 }
 
 // Options to replace infinite scrolling with a "Load more" button on selected elements
